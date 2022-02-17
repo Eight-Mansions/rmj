7b21d9d762b99e428ec6c98cd1a61b539c83a9ac
esperknight <esperknight@yahoo.com>
2022-02-16 22:35:38 -0500

    Redid how font was combined to not screw up palette...

--- code/rmj/subtitle/subtitle.c
+++ code/rmj/subtitle/subtitle.c
@@ -10,8 +10,10 @@ int GetLetterPos(char letter)
 	letter -= 0x41;
 	y = (letter / 13);
 	x = letter - (y * 13);
-	y *= 18;
-	x *= 16;
+	y *= 16;
+	x *= 8;
+
+	x += 208; // Offset to new font
 	return (y << 16 | x);
 }
 
@@ -22,11 +24,12 @@ int DisplaySubtitle()
 	8001f2c0 10 00 c2 af     sw         v0, local_10(s8)*/
 
 	char covid[5] = { 'C', 'O', 'V', 'I', 'D' };
+	//char covid[5] = { 'A', 'A', 'A', 'A', 'A' };
 	int textId = 0x0A;
 	int unk1 = 0; // Apparently always 0?
 	int yx = 0x50005c;
 	int uv = 0;
-	int wh = 0x00100010;
+	int wh = 0x00100008;
 	int unk3 = 0x19; // Is this the "order" on the screen?
 
 	int returnMe = 0; // Do I need to do this?
